4c96da2616a66cdbd7ed79bda45ac24ff8b2c2b6
Manmeet Maan <49202620+Maan2003@users.noreply.github.com>
2020-12-15 18:21:26 +0530

    add program to add, subtract and multiply numbers

---
+++ src/printf-scanf/arithmetic.c
@@ -0,0 +1,32 @@
+// The program to add, subtract and multiply two numbers, given by the user
+
+// include the io header, so that we can use printf and scanf
+#include<stdio.h>
+
+// execution of any program starts from `main` function
+int main() {
+    // declare two variables use to store the numbers
+    int a, b;
+    
+    // print to stdout
+    printf("Enter the value of a: ");
+    
+    // ask for user input (from stdin)
+    scanf("%d", &a);
+    
+    // same for b
+    printf("Enter the value of b: ");
+    scanf("%d", &b);
+    
+    // print the results
+    printf(
+        "The sum is %d. The difference is %d. The product is %d.",
+        a + b,
+        a - b,
+        a * b,
+    );
+    
+    // this is the exit code of our program
+    // 0 is convention for successful exectution of program
+    return 0;
+}
